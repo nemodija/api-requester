5b7aa8bde6aeb37fa04aaba2abd7e1defe5ddd21
ot-nemoto <nemoto@opentone.co.jp>
2020-11-12 03:52:30 +0000

    リクエスト一覧をyaml形式のファイルから読み込むように変更した

--- README.md
+++ README.md
@@ -17,45 +17,44 @@ python --version
 |--|--|--|
 |protocol|接続するプロトコル|`HTTP`|
 |hostname|接続先のホスト名|`localhost`|
-|request_path_list|APIのURLパス一覧を記述したファイルパス|`./request_path_list`|
+|request_path_file|APIのURLパス一覧を記述したファイルパス|`./request_path.yaml`|
 |result/parent_dir_path|結果出力先のディレクトリ|`.`|
 |result/dir_prefix|結果出力ディレクトリのプレフィックス|`result`|
 |placeholder|APIのURLパス一覧のプレースホルダーの正規表現|`(:\w+)`|
-|variables|プレースホルダーの置換一覧| *None* |
+|variables|プレースホルダーの置換一覧<br>request_list.yml| *None* |
 
 例)
 ```yaml
 protocol: https
 hostname: example.com
-request_path_list: './request_path_list'
+request_path_file: './request_list.yml'
 result:
   parent_dir_path: '.'
-  dir_prefix: 'EAMPLE'
+  dir_prefix: 'EXAMPLE'
 placeholder: '(:\w+)'
 variables:
-  ':prefCode':
-    - 1
-    - 2
-  ':cityCode':
-    - '01100'
-    - '02203'
-  ':year':
-    - 2005
-    - 2010
-    - 2015
+  'group-a':
+    ':prefCode':
+      - 1
+      - 2
+    ':cityCode':
+      - '01100'
+      - '02203'
+    ':year':
+      - 2005
+      - 2010
+      - 2015
 ```
 
-**request_path_list**
+**request_list.yml**
 
 例)
 
 ```
-/api/v1/cities/:prefCode
-# /api/v1/cities/:prefCode/:cityCode
-/api/v1/cities/:year/:prefCode/:cityCode
+group-a:
+- /api/v1/cities/:prefCode
+- /api/v1/cities/:year/:prefCode/:cityCode
 ```
-
-※行頭に `#` がある行はリクエストをスキップ
 
 ## Run
 
--- api-requester.py
+++ api-requester.py
@@ -28,48 +28,47 @@ result_dir_path = os.path.join(result_parent_dir_path, result_dir_name)
 placeholder = config.get('placeholder', r'(:\w+)')
 
 """
-Read request path list
+Read request path
 """
-request_path_list = config.get('request_path_list', './request_path_list')
-if not os.path.exists(request_path_list):
-    sys.exit()
+request_path_file = config.get('request_path_file', './request_path.yaml')
+if os.path.exists(request_path_file):
+    with open(request_path_file) as file:
+        request_path = yaml.safe_load(file.read()) or {}
+else:
+    request_path = {}
 
-with open(request_path_list, 'r') as f:
-    request_path = f.readline()
-    while request_path:
-        if re.match('^#', request_path):
-            request_path = f.readline()
-            continue
-        
-        print(request_path.strip())
+"""
+Request
+"""
+for key in request_path.keys():
+    for request_path in request_path.get(key):
+        print(request_path)
 
-        # プレースホルダ抽出
-        var_keys = re.findall(placeholder, request_path.strip())
+        # Extraction of placeholders
+        var_keys = re.findall(placeholder, request_path)
         variables = []
         for var_key in var_keys:
-            variables.append(config.get('variables', {}).get(var_key, []))
+            variables.append(config.get('variables', {}).get(key, {}).get(var_key, []))
+        print(variables)
 
         for val in list(itertools.product(*variables)):
-            # プレースホルダ置換
-            replaced_path = request_path.strip()
+            # Placeholder replacement
+            replaced_request_path = request_path
             for i, var_key in enumerate(var_keys):
-                replaced_path = replaced_path.replace(var_key, str(val[i]))
-            url = urllib.parse.urljoin(base_url, replaced_path)
-            print(url)
+                replaced_request_path = replaced_request_path.replace(var_key, str(val[i]))
+            url = urllib.parse.urljoin(base_url, replaced_request_path)
 
             # Create Resut Directory
-            response_dir = os.path.join(result_dir_path, re.sub('^/', '', replaced_path))
-            #print(response_dir)
+            response_dir = os.path.join(result_dir_path, re.sub('^/', '', replaced_request_path))
             os.makedirs(response_dir, exist_ok=True)
 
             # Request
             response = requests.get(url)
-            
-            # 結果出力
+            print('[{}] {}'.format(response.status_code, url))
+
+            # Output for result
             with open(os.path.join(response_dir, str(response.status_code)), mode='w') as rt:
                 try:
                     json.dump(json.loads(response.text), rt, ensure_ascii=False, indent=2, sort_keys=True)
                 except json.JSONDecodeError as e:
                     rt.write(response.text)
-
-        request_path = f.readline()
